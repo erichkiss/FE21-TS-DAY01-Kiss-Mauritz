ab44989ce5f43262d60be62705ba200048a10ccc
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-03-17 11:27:58 +0100

    basicEX2 done -matthias

--- basic_ex_1/script.js
+++ basic_ex_1/script.js
@@ -25,3 +25,28 @@ var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // 1. Print your full name in the browser (you will have an object that has 2 properties fName and lName).
 // 2. Print your first name 10 times in the browser using a forEach loop (don't forget that forEach loop work on arrays, so you can put the object in an array and repeat it). 
 // 3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.
+var object_name = [{
+        fName: "Matthias",
+        lName: "Mauritz"
+    }, {
+        fName: "Erich",
+        lName: "Kiss"
+    }];
+//first name printing function
+function printMatthias10() {
+    for (var i = 0; i < 5; i++) {
+        object_name.forEach(function () {
+            console.log(object_name[0].fName);
+        });
+    }
+}
+;
+printMatthias10();
+function printMauritz10() {
+    for (var i = 0; i < 5; i++) {
+        console.log(object_name[0].lName);
+    }
+}
+setTimeout(function () {
+    printMauritz10(); //need to use a callback function for setTimeOut(), can then call a function in that callback! :)
+}, 5000);
