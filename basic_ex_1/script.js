005bbff3c56f88b2e0e555ba2183302ea0211752
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-03-17 11:08:31 +0100

    added an alt method

--- basic_ex_1/script.js
+++ basic_ex_1/script.js
@@ -1,15 +1,27 @@
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // other method of defining the array
 // let numberArray : Array <number> = [1,2,3,4,5,6,7,8,9,10];
-function multiply() {
-    for (var _i = 0, numberArray_1 = numberArray; _i < numberArray_1.length; _i++) {
-        var i = numberArray_1[_i];
-        for (var _a = 0, numberArray_2 = numberArray; _a < numberArray_2.length; _a++) {
-            var j = numberArray_2[_a];
-            var product = j * i;
-            console.log("Calc: " + j + " x " + i + " = " + product);
-        }
-    }
-}
-;
-multiply();
+// #1 way to do it 
+// function multiply () {
+//     for (let i of numberArray) {
+//         for (let j of numberArray) {
+//         let product = j * i;
+//         console.log(`Calc: ${j} x ${i} = ${product}`);
+//         }
+//     }
+// };
+// #2 way to do it
+// function multiplyAlt(){
+//     for (let i = 0; i < numberArray.length; i++){
+//         for (let j = 0; j < numberArray.length; j++){
+//         let product = numberArray[i] * numberArray[j];
+//         console.log(`${product}`);
+//         }
+//     }
+// }
+// multiply();
+//multiplyAlt();
+// Basic ex 2
+// 1. Print your full name in the browser (you will have an object that has 2 properties fName and lName).
+// 2. Print your first name 10 times in the browser using a forEach loop (don't forget that forEach loop work on arrays, so you can put the object in an array and repeat it). 
+// 3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.
